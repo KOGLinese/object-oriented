2296d9347f3546e03dbfe5b3e194116e45464b61
KOGLinese <1076452761@qq.com>
2017-04-25 22:50:59 +0800

    vs的圆面积

---
+++ Project1/Project1/cirlce.cpp
@@ -0,0 +1,14 @@
+#include "sb.h"
+int main()
+{
+	double r=0;
+	double area = 0;
+	r = scanf();
+	area = cal(r);
+
+
+	print(area);
+	system("pause");
+	return 0;
+	
+}
---
+++ Project1/Project1/sb.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<iostream>
+#define pi 3.14
+using namespace std;
+double scanf()
+{
+	double r = 0;
+	cin >> r;
+	return r;
+}
+double cal(double x)
+{
+	double area = 0;
+	area = pi*x*x;
+	return area;
+}
+void print(double y)
+{
+	cout << y << endl;
+}
